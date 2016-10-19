b282b28997a8cf98a21db2c75db2ce1bf2483543
tucici <you@yourdomain.com>
2016-10-19 13:30:47 +0800

    Initial Commit

---
+++ CustomCollectionView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CustomCollectionView
+//
+//  Created by mac1 on 16/10/19.
+//  Copyright © 2016年 Tucici. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
